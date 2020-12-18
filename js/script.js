a35deef4b43cf80306acaecb36c47430e40025b4
Olena Bondarenko <olena.bondarenko28@gmail.com>
2020-12-18 17:17:44 +0100

    addclass for articles, href to links, active class to correct article

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
     console.log('links:', links);
 });*/
 const titleClickHandler = function(event) {
+    event.preventDefault();
     const clickedElement = this;
     console.log(event);
     /* [DONE] remove class 'active' from all article links  */
@@ -21,11 +22,14 @@ const titleClickHandler = function(event) {
     for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
-    /* get 'href' attribute from the clicked link */
-
-    /* find the correct article using the selector (value of 'href' attribute) */
-
-    /* add class 'active' to the correct article */
+    /* [DONE] get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+    /* [DONE] add class 'active' to the correct article */
+    targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
