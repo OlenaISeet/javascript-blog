ffcc747751b9d6d9d1e758c0fdcec9ede6de375f
Olena Bondarenko <olena.bondarenko28@gmail.com>
2020-12-18 17:05:20 +0100

    add class active to the clicked link

--- js/script.js
+++ js/script.js
@@ -4,17 +4,20 @@
     console.log('links:', links);
 });*/
 const titleClickHandler = function(event) {
+    const clickedElement = this;
     console.log(event);
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
-    /* add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
+    console.log('clickedElement:', clickedElement);
 
-    /* remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('.post a.active');
+    /* [DONE] remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.post article.active');
     for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
